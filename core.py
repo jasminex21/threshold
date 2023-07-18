f345bf9d3b4e51fef7017f8f1a17971c75bda171
mscaudill <mscaudill@gmail.com>
2023-07-17 19:58:31 -0500

    refactor: move threshold from core to masking since it makes a mask for a producer

--- core.py
+++ core.py
@@ -19,68 +19,6 @@ from openseize.file_io.annotations import Pinnacle
 from openseize.spectra import metrics, plotting
 
 
-def threshold(pro: Producer,
-              nstds: float,
-              chunksize: Optional[int]=None, axis:
-              int=-1
-) -> Producer:
-    """Returns a producer where normalized samples exceeding nstds has been
-    removed along axis.
-
-    Args:
-        pro:
-            A producer of ndarrays of data to threshold.
-        nstds:
-            A multiple of the data's standard deviation for determining extreme
-            values.
-        chunksize:
-            The number of samples the producer will produce during iteration.
-            This value determines the local mean and standard deviation for
-            thresholding.
-        axis:
-            The sample axis of the producer.
-
-    Examples:
-        >>> from openseize.file_io import edf
-        >>> from openseize import producer
-        >>> import numpy as np
-        >>> # make a random array with 50 spikes in each row
-        >>> rng = np.random.default_rng(0)
-        >>> x = rng.normal(loc=0, scale=1.0, size=(4,1000))
-        >>> locs = rng.choice(np.arange(1000), size=(4,50), replace=False)
-        >>> for row, loc_ls in enumerate(locs):
-        ...     x[row, loc_ls] = 10
-        >>> # make a producer from spiked data
-        >>> pro = producer(x, chunksize=100, axis=-1)
-        >>> masked_pro = threshold(pro, nstds=2)
-        >>> print(masked_pro.shape[-1] == pro.shape[-1] - len(np.unique(locs)))
-        True
-
-    Returns:
-        An openseize producer instance.
-    """
-
-    pro.chunksize = chunksize if chunksize else pro.chunksize
-
-    results = []
-    for idx, arr in enumerate(pro):
-
-        mu = np.mean(arr, axis=axis, keepdims=True)
-        std = np.std(arr, axis=axis, keepdims=True)
-
-        arr -= mu
-        arr /= std
-
-        # if exceeds in one channel we say exceeds in all
-        _, cols = np.where(np.abs(arr) > nstds)
-        cols += idx * pro.chunksize
-        results.append(np.unique(cols))
-
-    indices = np.concatenate(results)
-    mask = np.ones(pro.shape[pro.axis], dtype=bool)
-    mask[indices] = False
-    return producer(pro, pro.chunksize, pro.axis, mask=mask)
-
 def read_pinnacle(path: Union[str, Path],
                   labels: Sequence[str],
                   relative_to: Optional[str] = None,
