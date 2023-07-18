f95b6bdcacaaa98ceed83481b63eb969f7afd940
mscaudill <mscaudill@gmail.com>
2023-07-17 20:00:44 -0500

    refactor: move threshold from core to masking as masking is its purpose, also will require funcs that take readers to have the channels already set since this simplifies call sigs

--- masking.py
+++ masking.py
@@ -1,15 +1,77 @@
 import csv
 from functools import partial
 from itertools import zip_longest
-from typing import List, Union
+from typing import List, Optional, Union
 from pathlib import Path
 
 import numpy as np
+import numpy.typing as npt
 from openseize import producer
+from openseize.core.producer import Producer
 from openseize.file_io import annotations
 
 from threshold import core
 
+
+def threshold(pro: Producer,
+              nstds: float,
+              chunksize: Optional[int]=None,
+) -> Producer:
+    """Returns a producer where normalized samples exceeding nstds has been
+    removed along axis.
+
+    Args:
+        pro:
+            A producer of ndarrays of data to threshold.
+        nstds:
+            A multiple of the data's standard deviation for determining extreme
+            values.
+        chunksize:
+            The number of samples the producer will produce during iteration.
+            This value determines the local mean and standard deviation for
+            thresholding.
+
+    Examples:
+        >>> from openseize.file_io import edf
+        >>> from openseize import producer
+        >>> import numpy as np
+        >>> # make a random array with 50 spikes in each row
+        >>> rng = np.random.default_rng(0)
+        >>> x = rng.normal(loc=0, scale=1.0, size=(4,1000))
+        >>> locs = rng.choice(np.arange(1000), size=(4,50), replace=False)
+        >>> for row, loc_ls in enumerate(locs):
+        ...     x[row, loc_ls] = 10
+        >>> # make a producer from spiked data
+        >>> pro = producer(x, chunksize=100, axis=-1)
+        >>> masked_pro = threshold(pro, nstds=2)
+        >>> print(masked_pro.shape[-1] == pro.shape[-1] - len(np.unique(locs)))
+        True
+
+    Returns:
+        An openseize producer instance.
+    """
+
+    pro.chunksize = chunksize if chunksize else pro.chunksize
+    axis = pro.axis
+
+    results = []
+    for idx, arr in enumerate(pro):
+
+        mu = np.mean(arr, axis=axis, keepdims=True)
+        std = np.std(arr, axis=axis, keepdims=True)
+
+        arr -= mu
+        arr /= std
+
+        # if exceeds in one channel we say exceeds in all
+        _, cols = np.where(np.abs(arr) > nstds)
+        cols += idx * pro.chunksize
+        results.append(np.unique(cols))
+
+    indices = np.concatenate(results)
+    mask = np.ones(pro.shape[pro.axis], dtype=bool)
+    mask[indices] = False
+    return producer(pro, pro.chunksize, pro.axis, mask=mask)
 
 def state_mask(path, labels, fs, winsize, include=True, **kwargs):
     """Returns a boolean mask from a spindle sleep score text file.
@@ -47,7 +109,6 @@ def state_mask(path, labels, fs, winsize, include=True, **kwargs):
     result = np.repeat(mask, fs * winsize, axis=0)
     return result.flatten(order='F')
 
-
 def artifact_mask(path, size, labels, fs, relative_to, start=6, **kwargs):
     """Returns a boolean mask from a Pinnacle annotations file.
 
@@ -77,7 +138,6 @@ def artifact_mask(path, size, labels, fs, relative_to, start=6, **kwargs):
 
     annotes = core.read_pinnacle(path, labels, relative_to, start, **kwargs)
     return annotations.as_mask(annotes, size, fs, include=False)
-
 
 def _between_gen(reader, start, stop, chunksize, axis):
     """A generating function returns a generator of ndarrays of samples between
@@ -109,8 +169,7 @@ def _between_gen(reader, start, stop, chunksize, axis):
     for a, b in zip_longest(starts, starts[1:], fillvalue=stop):
         yield reader.read(a,b)
 
-
-def between_pro(reader, start, stop, channels, chunksize, axis=-1):
+def between_pro(reader, start, stop, chunksize, axis=-1):
     """Returns a producer from a reader instance that produces values between
     start and stop.
 
@@ -121,8 +180,6 @@ def between_pro(reader, start, stop, channels, chunksize, axis=-1):
             The index at which production of values from reader begins.
         stop:
             The index at which production of values from reader ends.
-        channels:
-            The channels that should be produced from reader.
         axis:
             The axis along which production should occur. Defaults to last axis.
 
@@ -131,9 +188,9 @@ def between_pro(reader, start, stop, channels, chunksize, axis=-1):
         >>> from openseize.file_io import edf
         >>> path = paths.locate('recording_001.edf')
         >>> reader = edf.Reader(path)
+        >>> reader.channels = [0,1,2]
         >>> # read samples 10 to 1155 in chunks of 100
-        >>> pro = between_pro(reader, 10, 1155, channels=[0, 1, 2, 3], 
-        ...                   chunksize=100)
+        >>> pro = between_pro(reader, 10, 1155, chunksize=100)
         >>> np.allclose(pro.to_array(), reader.read(10, 1155))
         True
 
