6a7ac16151f28793136bab93ab768a729a6fc25b
mscaudill <mscaudill@gmail.com>
2023-07-25 15:29:45 -0500

    dev: initial commit of multiprocessing of files, still needs docs

--- psds/script.py
+++ psds/script.py
@@ -1,48 +1,44 @@
-"""This script computes & stores the Power Spectral Densities of a collection
-of files using different voltage thresholds for artifact removal.
+"""This script computes the power spectral density for a collection of eeg files
+and their associated annotation and spindel state files. Specifically it
+constructs PSDs for each of the following conditions for each file:
 
-The primary question this project aims to address is how strongly do artifacts
-impact long-duration (24 hour) estimates of the PSD and is there an optimal
-local volatge threshold to remove these artifacts?
+    awake
+    sleep
+    awake + manually annotated
+    sleep + manually annotated
+    awake + threshold = 4 standard deviation
+    awake + threshold = 5
+    awake + threshold = 6
+    sleep + theshold = 4
+    sleep + theshold = 5
+    sleep + theshold = 6
 
+Before PSD computation, each file is preprocessed by notch filter 60 Hz and
+downsampling from 5 KHz to 250 Hz.
+
+Functions marked with an underscore are not intended for external calls.    
 """
 
+import pickle
+import time
+from functools import partial
 from itertools import zip_longest
+from pathlib import Path
+from multiprocessing import Pool
 
 from openseize import producer
-from openseize.file_io import edf
+from openseize.file_io import annotations, edf
 from openseize.filtering.iir import Notch
 from openseize.resampling import resampling
-from openseize.file_io import annotations, edf
+from openseize.spectra import estimators
 
-import time
-
+from threshold.tools import concurrency, timers
 from threshold import masking
 
-# PLAN
-# for an edf, annotation and corresponding spindle file
-# 1. create a between_pro
-# 2. Notch filter the pro
-# 3. Downsample the pro
-#
-# 4. Create masks the combination of (awake, artifact) and (sleep, artifact)
-# 5. Mask the downsampled pro with (awake, artifact) mask & (sleep, artifact)
-#    mask
-# 6. create thresholded producers in awake and sleep states on per threshold
-# 
-# 7. take downsampled pro and mask with manual artifacts -> man_pro
-# 8. split man_pro into awake (zx) and sleep (zy) producers
-# 9. Finally compute psd estimates of each producer and store
-#
 
-
-def preprocess(path, annotes, start, stop, fs, M, chunksize=30e5, axis=-1):
-    """Notch filters and downsamples data produced from a reader between start
-    and stop annotations.
-
-
-    # Fix if start or stop is None
-    """
+def _preprocess(path, annotes, start, stop, fs, M, chunksize=30e5, axis=-1):
+    """Returns a producer producing chunksize samples along axis that has been
+    notch filtered at 60 Hz and downsampled from fs to fs/M."""
 
     reader = edf.Reader(path)
     reader.channels = [0, 1, 2]
@@ -58,70 +54,164 @@ def preprocess(path, annotes, start, stop, fs, M, chunksize=30e5, axis=-1):
     return result
 
 
-def build_masks(epath, apath, spath, nstds=[4,5,6]):
-    """ """
+def _masks(epath, apath, spath, nstds=[4,5,6], verbose=False):
+    """Returns a list of Metamask instances for the following conditions:
 
-    # use of this function before a psd func will mean data is transversed 2X
-
+    awake
+    sleep
+    awake + manually annotated
+    sleep + manually annotated
+    awake + threshold = 4 standard deviation
+    awake + threshold = 5
+    awake + threshold = 6
+    sleep + theshold = 4
+    sleep + theshold = 5
+    sleep + theshold = 6
+    
+    Args:
+        epath:
+            Path to an eeg file
+        apath:
+            Path to annotation file associated with eeg at epath
+        spath:
+            Path to a spindle state file associated with eeg at epath.
+        verbose:
+            If True prints timing information for mask construction.
+    """
 
     t0 = time.perf_counter()
+
+    # read annotations file to list of annote instances.
+    with annotations.Pinnacle(apath, start=6) as reader:
+        annotes = reader.read()
+   
+    # proprocess file
+    pro = _preprocess(epath, annotes, 'Start', 'Stop', fs=5000, M=20)
+
+    # threshold preprocessed by computing a boolean for each std in nstds
+    thresholds = masking.threshold(pro, nstds, chunksize=1.5e4)
+
+    # construct manually annotated boolean
+    annote = masking.artifact(apath, size=pro.shape[-1],
+                        labels=[ 'Artifact', 'Artifact ', 'water', 'water '],
+                        fs=250, between=['Start', 'Stop'])
+    
+    # build awake and sleep booleans
+    awake = masking.state(spath, ['w'], fs=250, winsize=4)
+    sleep = masking.state(spath, ['r', 'n'], fs=250, winsize=4)
+
+    metamasks = []
+    # build awake and threshold metamasks
+    for a, b, std in zip_longest([awake], thresholds, nstds, fillvalue=awake):
+        names = ['awake', f'threshold = {std}']
+        metamasks.append(masking.MetaMask([a, b],  names))
+
+    # build sleep and threshold metamasks
+    for a, b, std in zip_longest([sleep], thresholds, nstds, fillvalue=sleep):
+        names = ['sleep', f'threshold = {std}']
+        metamasks.append(masking.MetaMask([a, b],  names))
+
+    # build awake and sleep with manual artifacts removed
+    for a, b, names in [(awake, annote, ['awake', 'annote']),
+                        (sleep, annote, ['sleep', 'annote'])]:
+        metamasks.append(masking.MetaMask([a, b], names))
+
+    # build awake and sleep without artifact removal
+    for mask, name in zip([awake, sleep], ['awake', 'sleep']):
+        metamasks.append(masking.MetaMask([mask], [name]))
+        
+    if verbose:
+        print(f'build masks completed in {time.perf_counter() - t0} s')
+
+    return metamasks
+
+
+def process_file(epath, apath, spath, nstds, verbose=False):
+    """Returns a nested dictionary with animal name as the outer key and
+    a condition tuple as the inner key storing openseize psd tuple results."""
+
+    t0 = time.perf_counter()
+    metamasks = _masks(epath, apath, spath, nstds)
+
     # open annotes
     with annotations.Pinnacle(apath, start=6) as reader:
         annotes = reader.read()
    
     # proprocess file
-    pro = preprocess(epath, annotes, start='Start', stop='Stop',
+    pro = _preprocess(epath, annotes, start='Start', stop='Stop',
                      fs=5000, M=20)
 
-    # threshold masks
-    thresholds = masking.threshold(pro, nstds, chunksize=1.5e4)
+    masked_pros = [producer(pro, chunksize=30e5, axis=-1, mask=m.mask) 
+                   for m in  metamasks]
 
-    
-    # manual artifact mask
-    annote = masking.artifact(apath, size=pro.shape[-1],
-                        labels=[ 'Artifact', 'Artifact ', 'water', 'water '],
-                        fs=250, between=['Start', 'Stop'])
-    
-    # build state masks
-    awake = masking.state(spath, ['w'], fs=250, winsize=4)
-    sleep = masking.state(spath, ['r', 'n'], fs=250, winsize=4)
+    psd_tups = [estimators.psd(mpro, fs=250) for mpro in masked_pros] 
 
-    metamasks = []
-    # build awake and threshold masks
-    for a, b, std in zip_longest([awake], thresholds, nstds, fillvalue=awake):
-        names = ['awake', f'threshold = {std}']
-        results.append(masking.MetaMask([a, b],  names))
+    results = [[tuple(m.submasks.keys()), psd_tup] for m, psd_tup in 
+                zip(metamasks, psd_tups)]
 
-    # build sleep and threshold masks
-    for a, b, std in zip_longest([sleep], thresholds, nstds, fillvalue=sleep):
-        names = ['sleep', f'threshold = {std}']
-        results.append(masking.MetaMask([a, b],  names))
+    name = epath.stem.split('_')[0]
+    conds = [tuple(m.submasks.keys()) for m in metamasks]
+    results = {name: dict(zip(conds, psd_tups))}
 
-    # build awake and annotated as well as sleep and annotated
-    for a, b, names in [(awake, annote, ['awake', 'annote']),
-                        (sleep, annote, ['sleep', 'annote'])]:
-        results.append(masking.MetaMask([a, b], names))
-
-    # build awake and sleep with artifacts in place
-    for mask, name in zip([awake, sleep], ['awake', 'sleep']):
-        results.append(masking.MetaMask([mask], [name]))
-        
-
-    print(f'build masks completed in {time.perf_counter() - t0} s')
+    print(f'PSD estimation for file {name} complete')
 
     return results
 
+def process_files(dirpaths, save_path, nstds, ncores=None):
+    """ """
+
+    t0 = time.perf_counter()
+    results = [{} for _ in range(len(dirpaths))]
+    for result, dirpath in zip(results, dirpaths):
+
+        epaths = list(Path(dirpath).glob('*.edf'))
+        apaths = list(Path(dirpath).glob('*.txt'))
+        spaths = list(Path(dirpath).glob('*.csv'))
+
+        workers = concurrency.set_cores(ncores, len(epaths))
+
+        # fix stds with partial
+        f = partial(process_file, nstds=[4,5,6])
+        with Pool(workers) as pool:
+            processed = pool.starmap(f, timers.Clockit(list(zip(epaths, apaths,
+                spaths))))
+
+        result.update(processed[0])
+
+    # save data
+    with open(Path(save_path).joinpath('psds.pkl'), 'wb') as outfile:
+        pickle.dump(result, outfile)
+
+    print(f'processed {len(epaths)} files in {time.perf_counter() - t0} s')
+
+    return result
+
+
 if __name__ == '__main__':
 
-    from pathlib import Path
-
+    """
     base_path = Path('/media/matt/Zeus/jasmine/stxbp1/')
     name = 'CW0DA1_P096_KO_15_53_3dayEEG_2020-04-13_08_58_30'
     epath = base_path.joinpath(name + '.edf')
     apath = base_path.joinpath(name + '.txt')
     spath = base_path.joinpath(name + '_sleep_states.csv')
 
-    metamasks = build_masks(epath, apath, spath)
+    #results = process_single(epath, apath, spath, nstds=[4,5,6])
+
+    #metamasks = _masks(epath, apath, spath, verbose=True)
+
+    #psd_dict = process_file(epath, apath, spath, nstds=[4,5,6], verbose=True)
+    """
+    
+    dirpaths = ['/media/matt/Zeus/jasmine/stxbp1_test/']
+    save_path = '/media/matt/Zeus/jasmine/results'
+
+    psds = process_files(dirpaths, save_path=save_path, nstds=[4,5,6])
+
+
+
+    
+
 
     
 
